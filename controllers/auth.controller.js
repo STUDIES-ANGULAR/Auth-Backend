3d973f9e079fd95361133e3706564fe967f3b342
ArleyRivasG <arrivasg@unipacifico.edu.co>
2022-12-27 22:18:57 -0500

    Crear usuario en base de datos

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,51 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
 
 
-const crearUsuario = (req, res= response) => {
+const crearUsuario = async (req, res = response) => {
 
     const { name, email, password } = req.body;
 
-    return res.json({
-        ok: true,
-        msg: 'Crear usuario /new'
-    });
+    try{
+        //Verificar el email
+        const usuario = await Usuario.findOne({ email });
+
+        if ( usuario ) {
+            return  res.status(400).json({
+                ok: false,
+                msg: 'Ya existe un usuario con ese email'
+            })
+        }    
+
+        //Crear usuario con el modelo
+        const dbUser = new Usuario( req.body );
+        
+        // Hashear la contraseña
+
+
+        //Generar el JWT
+
+      
+        //Crear usuario de DB
+        await dbUser.save();
+
+        //Generar respuesta exitosa
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name, 
+        });
+
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+
+    }
+
 
 }
 
